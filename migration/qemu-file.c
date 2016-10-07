4ef161cbf2d4f1097cbc39ed217c8ebbe35a1bd3
Christian Borntraeger <borntraeger@de.ibm.com>
2016-10-07 15:04:57 +0200

    o3
    make: Entering directory '/home/cborntra/REPOS/qemu/build'
      CC      migration/qemu-file.o
    /home/cborntra/REPOS/qemu/migration/qemu-file.c: In function
    ‘qemu_get_buffer_in_place’:
    /home/cborntra/REPOS/qemu/migration/qemu-file.c:490:18: error: ‘src’ may
    be used uninitialized in this function [-Werror=maybe-uninitialized]
                 *buf = src;
                      ^
    cc1: all warnings being treated as errors
    /home/cborntra/REPOS/qemu/rules.mak:60: recipe for target
    'migration/qemu-file.o' failed
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: <developer> <email address>

--- migration/qemu-file.c
+++ migration/qemu-file.c
@@ -479,7 +479,7 @@ size_t qemu_get_buffer(QEMUFile *f, uint8_t *buf, size_t size)
  */
 size_t qemu_get_buffer_in_place(QEMUFile *f, uint8_t **buf, size_t size)
 {
-    if (size < IO_BUF_SIZE) {
+    if (size > 0 && size < IO_BUF_SIZE) {
         size_t res;
         uint8_t *src;
 
